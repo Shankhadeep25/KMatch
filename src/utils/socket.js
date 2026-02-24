669fe5ce9e288fd2a00511c860ecd530f42766fd
Shankhadeep25 <shankhadeepdey251@gmail.com>
2026-02-24 22:55:09 +0530

    chat existingconn added

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,7 @@
-const socket = require("socket.io")
+const socket = require("socket.io");
+const crypto = require("crypto");
+const { Chat } = require("../models/chat");
+const { connectionRequest } = require("../models/connectionRequest")
 
 const getSecureRoomId = (userId, targetUserId) => {
     return crypto.createHash("sha256").update([userId, targetUserId].sort().join("_"))
@@ -19,10 +22,51 @@ const initializeSocket = (server) => {
             socket.join(roomId)
         })
 
-        socket.on("sendMessage", ({firstName, userId, targetUserId, photo ,text}) => {
-            const roomId = getSecureRoomId(userId, targetUserId);
-            console.log(firstName + "" + text)
-            io.to(roomId).emit("messageReceived", {firstName,photo ,text})
+        socket.on("sendMessage", async ({firstName, userId, targetUserId, photo ,text}) => {
+            //save message to DB
+            try{
+                const roomId = getSecureRoomId(userId, targetUserId);
+                console.log(firstName + "" + text);
+
+                const existingConn = await connectionRequest.findOne({
+                    $or:[
+                        {fromUserId: userId , toUserId: targetUserId},
+                        {fromUserId: userId , toUserId: targetUserId},
+                    ],
+                    status: "accepted",
+            })
+
+            if(!existingConn){
+                console.log("Unauthorized chat attempt")
+                return socket.emit("errorMessage", "You are not connected with this user!")
+            }
+
+                let chat = await Chat.findOne({
+                    participants: {$all: [userId, targetUserId]}
+                })
+
+                if(!chat){
+                    chat = new Chat({
+                        participants: [userId, targetUserId],
+                        messages: []
+                    })
+                }
+
+                chat.messages.push({
+                    senderId: userId,
+                    text
+                })
+
+                await chat.save()
+                io.to(roomId).emit("messageReceived", {
+                    firstName,
+                    photo,
+                    text,
+                });
+            }
+            catch (err) {
+                console.error(err)
+            }
         })
 
         socket.on("disconnect", () => {
