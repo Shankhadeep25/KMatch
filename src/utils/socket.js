5fda5f7ff3a8ba42c6afc48d1d3f648a4d9d5ec0
Shankhadeep25 <shankhadeepdey251@gmail.com>
2026-02-24 16:00:32 +0530

    chat feature

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,23 +1,32 @@
 const socket = require("socket.io")
 
+const getSecureRoomId = (userId, targetUserId) => {
+    return crypto.createHash("sha256").update([userId, targetUserId].sort().join("_"))
+    .digest("hex");
+}
+
 const initializeSocket = (server) => {
     const io = socket(server, {
-      cors: {
+        cors: {
         origin: "http://localhost:5173",
-      },
+        },
     });
 
     io.on("connection", (socket) => {
-        socket.on("joinChat", () => {
-
+        socket.on("joinChat", ({firstName, userId, targetUserId}) => {
+            const roomId = getSecureRoomId(userId, targetUserId)
+            console.log(firstName + "Joined Room: " + roomId)
+            socket.join(roomId)
         })
 
-        socket.on("sendMessage", () => {
-
+        socket.on("sendMessage", ({firstName, userId, targetUserId, photo ,text}) => {
+            const roomId = getSecureRoomId(userId, targetUserId);
+            console.log(firstName + "" + text)
+            io.to(roomId).emit("messageReceived", {firstName,photo ,text})
         })
 
         socket.on("disconnect", () => {
-            
+
         })
     })
 }
